af0fe893ef1276c209db612d3116e84a2e041a8b
Erson1989x <Erson1989x@gmail.com>
2026-02-12 21:48:55 +0200

    fix

--- views/404.php
+++ views/404.php
@@ -5,7 +5,7 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">This is the 404 page</h1>
         <p class="mt-4">Sorry, the page you are looking for does not exist.</p>
-        <p  class="mt-4">You can go back to the <a href="/">Home page</a>.</p>
+        <p  class="mt-4">You can go back to the <a href="/" class="underline font-bold">Home page</a>.</p>
     </div>
 </main>
 <?php require 'partials/footer.php'; ?>
