82ccbc6003f7f7fa4815668b8f61920fb0c1e8d5
Erson1989x <Erson1989x@gmail.com>
2026-02-25 21:08:37 +0200

    progress

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -3,13 +3,19 @@
 <?php require 'views/partials/banner.php'; ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <button><a href="/notes" class="text-blue-500 underline mb-4 rounded-lg bg-blue-50 px-4 py-2">Go Back</a></button>
-        <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">
-            <div class="px-4 py-5 sm:px-6">
-                <p class="mt-1 max-w-2xl text-sm text-gray-500"><?= htmlspecialchars($note['body']); ?></p>
+        <button class="text-blue-500 underline mb-4 rounded-lg bg-blue-50 px-4 py-2 border border-blue-400 hover:bg-blue-100"><a href="/notes/create" class="text-blue-500 no-underline">New Note</a></button>
+
+        <?php foreach ($notes as $note) : ?>
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">
+                <div class="px-4 py-5 sm:px-6">
+                    <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                            <?= htmlspecialchars($note['body']) ?>
+                        </a>
+                    </p>
+                </div>
             </div>
-        </div>
-    </div>
+        <?php endforeach; ?>
     </div>
 </main>
 <?php require 'views/partials/footer.php'; ?>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -3,15 +3,11 @@
 <?php require 'views/partials/banner.php'; ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <?php foreach ($notes as $note) : ?>
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4 w-1/3">
-                <div class="px-4 py-5 sm:px-6">
-                    <p class="mt-1 max-w-2xl text-sm text-gray-500"><a class="underline text-blue-500" href="note?id=<?= $note['id']; ?>"><?=htmlspecialchars( $note['body']); ?></a></p>
-                </div>
-            </div>
-        <?php endforeach; ?>
+        <p class="mb-6">
+            <a href="/notes" class="text-blue-500 underline">Go Back...</a>
+        </p>
 
-        <button class="text-blue-500 underline mb-4 rounded-lg bg-blue-50 px-4 py-2 border border-blue-400 hover:bg-blue-100"><a href="/note/create" class="text-blue-500 underline mb-4 rounded-lg bg-blue-50 px-4 py-2">New Note</a></button>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 <?php require 'views/partials/footer.php'; ?>
